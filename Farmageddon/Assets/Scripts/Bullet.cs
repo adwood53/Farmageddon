e96038ca0a18ca662afda76fecd6849ea06962b6
iChocChip <ashtoncarter3769@gmail.com>
2018-11-03 17:18:25 +0000

    Added more to gun

--- Farmageddon/Assets/Scripts/Bullet.cs
+++ Farmageddon/Assets/Scripts/Bullet.cs
@@ -7,15 +7,18 @@ public class Bullet : MonoBehaviour {
     GunWeapon gunBullet;
     public Sprite currentBullet;
     mouseCursor cursor;
+
+    public float speed;
 	// Use this for initialization
 	void Start ()
     {
         currentBullet = gunBullet.gameBullet;
+        speed = gunBullet.speed;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    	
+	    
 	}
 }
--- Farmageddon/Assets/Scripts/WeaponSpawner.cs
+++ Farmageddon/Assets/Scripts/WeaponSpawner.cs
@@ -4,17 +4,53 @@ using UnityEngine;
 
 public class WeaponSpawner : MonoBehaviour {
 
+
+    public GunWeapon currentGunWeapon;
     public float fireRate;
     public uint damage;
     public float speed;
 
-    GunWeapon currentGunWeapon;
+
+    private uint tier;
+    private uint newTier;
+    private Sprite gunWeapon;
+    private Sprite gameBullet;
+
+    public Bullet bullet;
+
+    private bool isUpgraded = false; 
 
     void Start()
     {
+        gunWeapon = currentGunWeapon.gunWeapon;
+        gameBullet = currentGunWeapon.gameBullet;
+
         fireRate = currentGunWeapon.fireRate;
         damage = currentGunWeapon.damage;
         speed = currentGunWeapon.speed;
+
+        tier = currentGunWeapon.tier;
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    void ChangeWeapon()
+    {
+    
+    }
+
+    void ChangeTier()
+    {
+        newTier = tier + 1;
+        
+        if(isUpgraded)
+        {
+            currentGunWeapon = new GunWeapon
+        }
+
     }
 
 }
