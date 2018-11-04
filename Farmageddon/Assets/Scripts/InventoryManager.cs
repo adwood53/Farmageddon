4ff1778eae82bf33ed21141a52867b753a8fd5ed
0shu <33581844+0shu@users.noreply.github.com>
2018-11-04 11:54:57 +0000

    Started work on inventory manager.
    Comminting so I can move downstairs

---
+++ Farmageddon/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour {
+
+	public int currentGuntTier;
+	public int currentMeleeTier;
+	public int currentGrenadeTier;
+
+	public enum selectedWeapon
+	{
+		gun,
+		melee,
+		grenades
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
