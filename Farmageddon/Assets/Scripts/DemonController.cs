b5107182a39c8d9a6635785c09a54444b2606baa
Aaron <Aaronaryat@gmail.com>
2018-11-04 12:43:18 +0000

    AI done

---
+++ Farmageddon/Assets/Scripts/DemonController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonController : MonoBehaviour {
+
+    private Transform Demon;
+    public Transform Target;
+
+    public float Dist;
+    private float yDis;
+    private float xDis;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Demon = gameObject.GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        yDis = Mathf.Abs(Target.position.y - Demon.position.y);
+        xDis = Mathf.Abs(Target.position.x - Demon.position.x);
+
+        if (yDis < Dist && xDis < Dist)
+        {
+            gameObject.GetComponent<Pathfinding.AIPath>().enabled = false;
+        }
+
+        else
+        {
+            gameObject.GetComponent<Pathfinding.AIPath>().enabled = true;
+        }
+	}
+}
+
+//if demon position == target.position +- 3
