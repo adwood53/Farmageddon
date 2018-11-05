0f1160631678cf7187301dbf5e0ef14ec566b67d
28 Brazil Street - Home PC <sunthlower@gmail.com>
2018-11-05 14:50:59 +0000

    Added Enemy Activator Script

---
+++ Farmageddon/Assets/Scripts/EnemyActivator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyActivator : MonoBehaviour 
+{
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Enemy")
+		{
+			AIDestinationSetter aIDestination = col.gameObject.GetComponent<AIDestinationSetter>();
+			aIDestination.enabled = true;
+		}
+	}
+	
+}
