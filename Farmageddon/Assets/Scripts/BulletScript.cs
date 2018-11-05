a656aceaf5f854b9507babdca98526df7abd28da
AJ <33582592+adwood53@users.noreply.github.com>
2018-11-05 16:14:54 +0000

    Corrected textures and enemies and added screenshake

--- Farmageddon/Assets/Scripts/BulletScript.cs
+++ Farmageddon/Assets/Scripts/BulletScript.cs
@@ -5,6 +5,12 @@ public class BulletScript : MonoBehaviour {
 
     public float damage;
     // Update is called once per frame
+
+    void Start()
+    {
+        Camera.main.GetComponentInChildren<cameraPositioner>().noiseFrequency = 20;
+    }
+
     void Update () {
         Vector2 screenPositiony = Camera.main.WorldToScreenPoint(transform.position);
         if (screenPositiony.y > Screen.height || screenPositiony.y < 0)
@@ -13,5 +19,14 @@ public class BulletScript : MonoBehaviour {
         Vector2 screenPositionx = Camera.main.WorldToScreenPoint(transform.position);
         if (screenPositionx.x > Screen.width || screenPositionx.x < 0)
             Destroy(this.gameObject);
+
+            
+    }
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            DestroyObject(gameObject);
+        }
     }
 }
--- Farmageddon/Assets/Scripts/cameraPositioner.cs
+++ Farmageddon/Assets/Scripts/cameraPositioner.cs
@@ -1,22 +1,36 @@
 ﻿using UnityEngine;
+using Cinemachine;
 
 public class cameraPositioner : MonoBehaviour {
 
  	public float closeness;
+
+	 [SerializeField]public float noiseFrequency;
+	 public float frequencyCoolDown;
     public Camera camera;
 	public GameObject cursor;
 	public GameObject character;
 	public GameObject centerPoint;
+	public CinemachineVirtualCamera _cinemachine;
 	Vector3 point;
 
 	// Use this for initialization
 	void Start () {
 		point = new Vector3();
+		_cinemachine = gameObject.GetComponent<CinemachineVirtualCamera>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		_cinemachine.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = noiseFrequency;
+		if(noiseFrequency > 0.1)
+		{
+			noiseFrequency -= frequencyCoolDown;
+		}
+		else
+		{
+			noiseFrequency = 0;
+		}
         point = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.nearClipPlane));
 		cursor.transform.position = point;
 
