eb08017c13667e0dfc556a9c8d66b7a3e321c458
0shu <33581844+0shu@users.noreply.github.com>
2018-11-17 14:47:49 +0000

    Added Basic Menus
    Also made scenechangers

---
+++ Farmageddon/Assets/Scripts/MoveScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour {
+
+	public void sceneChange (string sceneName)
+	{
+		Debug.Log("Changing Scene!");
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void quitGame ()
+	{
+		Application.Quit();
+	}
+}
