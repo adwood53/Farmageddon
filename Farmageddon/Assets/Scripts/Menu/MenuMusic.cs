6633525f854f6e4ddea3e80177fdd8fcff463d03
0shu <33581844+0shu@users.noreply.github.com>
2018-11-18 00:25:22 +0000

    Better Menu System + Music
    RAGEMODE MENU

---
+++ Farmageddon/Assets/Scripts/Menu/MenuMusic.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusic : MonoBehaviour {
+
+	public AudioSource music1;
+	public AudioSource music2;
+	public float fadeTime = 2;
+
+	private bool active = true;
+	private float elapsedTime = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		elapsedTime = fadeTime;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(elapsedTime <= fadeTime)
+		{
+			elapsedTime += Time.deltaTime;
+
+			if(active)
+			{
+				//swaps from music 2 to music 1
+				music1.volume = elapsedTime / fadeTime;
+				music2.volume = 1 - music1.volume;
+			}
+			else
+			{
+				//swaps from music 1 to music 2
+				music2.volume = elapsedTime / fadeTime;
+				music1.volume = 1 - music2.volume;
+			}
+		}
+	}
+
+	public void SwapMusic ()
+	{
+		elapsedTime = 0;
+		active = !active;
+	}
+}
---
+++ Farmageddon/Assets/Scripts/Menu/TextColor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextColor : MonoBehaviour {
+
+	public Color color1;
+	public Color color2;
+	public float fadeTime = 2;
+
+	private bool active = true;
+	private float elapsedTime = 0;
+	private Text text;
+	private Color colorDifference;
+	// Use this for initialization
+	void Start () 
+	{
+		text = gameObject.GetComponent<Text>();
+		elapsedTime = fadeTime;	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		colorDifference = color1 - color2;
+		if(elapsedTime <= fadeTime)
+		{
+			elapsedTime += Time.deltaTime;
+
+			if(active)
+			{
+				//swaps from color2 to color1
+				text.color = color2 + (colorDifference * (elapsedTime / fadeTime));
+			}
+			else
+			{
+				//swaps from color1 to color2
+				text.color = color1 - (colorDifference * (elapsedTime / fadeTime));
+			}
+		}
+	}
+
+	public void SwapColor ()
+	{
+		elapsedTime = 0;
+		active = !active;
+	}
+}
